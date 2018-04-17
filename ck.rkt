53de4cd1ae8e6637c097c7423c6b9ea4a902d945
Roger Jungemann <roger@thefifthcircuit.com>
2018-04-17 10:02:20 -0700

    WIP

--- ck.rkt
+++ ck.rkt
@@ -4,9 +4,19 @@
 
 (provide ck)
 
+#| ; TODO: Make this hygenic! |#
+#| (define-macro (ck . body) |#
+#|   (let ([n (make-base-namespace)]) |#
+#|     (parameterize ([current-namespace n]) |#
+#|       (namespace-require "ck-dsl.rkt")) |#
+#|     `(eval '(do ,@body) ,n))) |#
+
 ; TODO: Make this hygenic!
-(define-macro (ck . body)
+(define-macro (ck body)
   (let ([n (make-base-namespace)])
     (parameterize ([current-namespace n])
       (namespace-require "ck-dsl.rkt"))
-    `(eval '(do ,@body) ,n)))
+    `(eval ,body ,n)))
+
+#| (define n 1) |#
+#| (ck `(do (<<<>>> (int 0) (int ,n)))) |#
